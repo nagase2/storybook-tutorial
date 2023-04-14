8b28f671417a4a9cc97e6fc3ca4c9492461bb631
Nagase Yoshimasa <yoshimasa_nagase@tsyscom.co.jp>
2023-04-15 01:18:02 +0900

    a

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.js"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
   ],
   framework: "@storybook/react",
   core: {
